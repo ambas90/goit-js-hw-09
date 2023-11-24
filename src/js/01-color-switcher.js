8f3504fd0f47bf26e28986433a205b9e541c3c6b
Dawid Jasiński <ambas90@gmail.com>
2023-11-24 21:03:30 +0100

    task 1 and 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,8 @@
-const startBt = document.querySelector('button[data-start]');
-const stopBt = document.querySelector('button[data-stop]');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 let timer = null;
-stopBt.disabled = true;
+stopBtn.disabled = true;
 
 //generator losowych kolorów w formacie #
 function getRandomHexColor() {
@@ -15,17 +15,17 @@ function bodyColorChanger() {
   timer = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-  startBt.disabled = true;
-  stopBt.disabled = false;
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
 }
 
 //zatrzymanie zmieniania kolorów tła, odblokowanie buttona
 function ColorChangerStop() {
   clearInterval(timer);
-  startBt.disabled = false;
-  stopBt.disabled = true;
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
 }
 
 //nasłuchiwanie kliknięcia na przyciski
-startBt.addEventListener('click', bodyColorChanger);
-stopBt.addEventListener('click', ColorChangerStop);
+startBtn.addEventListener('click', bodyColorChanger);
+stopBtn.addEventListener('click', ColorChangerStop);
---
+++ src/js/02-timer.js
@@ -0,0 +1,105 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const timerContainer = document.querySelector('.timer');
+const fields = document.querySelectorAll('.field');
+const values = document.querySelectorAll('.value');
+const labels = document.querySelectorAll('.label');
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds');
+
+let selectedDate;
+let timer;
+
+//dezaktywacja przycisku start
+startBtn.disabled = true;
+
+//stylowanie elementów
+timerContainer.style.display = 'flex';
+timerContainer.style.gap = '20px';
+timerContainer.style.marginTop = '10px';
+for (const field of fields) {
+  field.style.display = 'flex';
+  field.style.flexDirection = 'column';
+  field.style.alignItems = 'center';
+}
+for (const value of values) {
+  value.style.fontSize = '30px';
+}
+for (const label of labels) {
+  label.style.textTransform = 'uppercase';
+  label.style.fontSize = '10px';
+  label.style.fontWeight = '600';
+}
+
+//Opcje biblioteki flatpickr
+//Sprawdzanie czy zostałą wybrana darta w przyszłości
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const currentDate = new Date();
+    if (currentDate > selectedDates[0]) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      return;
+    } else {
+      startBtn.disabled = false;
+      return (selectedDate = selectedDates[0]);
+    }
+  },
+};
+
+//inicjalizacja biblioteki flatpickr
+flatpickr(input, options);
+
+//funckja odliczająca i wyświetlająca czas pozostały do końca
+function countdown() {
+  const currentDate = new Date();
+  let timeDifference = selectedDate - currentDate;
+  if (timeDifference <= 0) {
+    clearInterval(timer);
+    return;
+  }
+  days.innerText = addLeadingZero(convertMs(timeDifference).days);
+  hours.innerText = addLeadingZero(convertMs(timeDifference).hours);
+  minutes.innerText = addLeadingZero(convertMs(timeDifference).minutes);
+  seconds.innerText = addLeadingZero(convertMs(timeDifference).seconds);
+}
+
+// funkcja dodająca zero jeżeli liczba jest jednocyfrowa
+function addLeadingZero(value) {
+  value = value.toString();
+  return value.padStart(2, '0');
+}
+
+// funkcja konwertująca milisekundy na dni,godziny,minuty,sekundy
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+//nasłuchiwanie kliknięcia na przycisk start
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  countdown();
+  timer = setInterval(countdown, 1000);
+});
