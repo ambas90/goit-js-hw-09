74a2511f05a1728ca705d7033c46283ced39eb64
Dawid Jasiński <ambas90@gmail.com>
2023-11-26 12:11:55 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,20 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const form = document.querySelector('.form');
+
+//wywoływanie funkcji tworzenia obietnic
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log('34534534');
 }
+
+//tworzenie obietnic
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+//nasłuchiwanie formularza
+form.addEventListener('submit', handleSubmit);
