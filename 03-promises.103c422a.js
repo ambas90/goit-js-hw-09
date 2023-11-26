877d665b2fa5e02b8ff6b0c7879739c5d51a98d9
ambas90 <ambas90@gmail.com>
2023-11-26 11:12:24 +0000

    Deploying to gh-pages from @ ambas90/goit-js-hw-09@74a2511f05a1728ca705d7033c46283ced39eb64 🚀

---
+++ 03-promises.103c422a.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault(),console.log("34534534")}));
+//# sourceMappingURL=03-promises.103c422a.js.map
---
+++ 03-promises.8e681204.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault(),console.log("34534534")}));
+//# sourceMappingURL=03-promises.8e681204.js.map
